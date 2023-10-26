94b73975f474071670b63e7a511421fe1b37336b
simona721 <simona94.sa@gmail.com>
2023-10-26 23:33:24 +0200

    Multiple inputs

---
+++ main.py
@@ -0,0 +1,18 @@
+
+#Taking two imputs at the same time
+x, y = input("Enter your inputs:").split()
+print(f"The input for value x is {x}")
+print(f"The input for value y is {y}")
+
+#Taking 3 values at the same time
+a, b, c = input("Enter your values: ").split()
+print(f"The value for a is {a}")
+print(f"The value for a is {b}")
+print(f"The value for a is {c}")
+
+#Taking multiple inputs at the same time
+x = [x for x in input("Enter values: ").split()]
+print(f"The values are: {x}")
+
+
+
